efd319b7c039340f554b5123452393a8993ae773
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2025-08-20 18:22:05 +0530

    15th August

--- src/main/java/com/thetestingacademy/modules/PayloadManager.java
+++ src/main/java/com/thetestingacademy/modules/PayloadManager.java
@@ -1,4 +1,94 @@
 package com.thetestingacademy.modules;
 
+import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+import com.thetestingacademy.pojos.requestPojos.Booking;
+import com.thetestingacademy.pojos.requestPojos.BookingDates;
+import com.thetestingacademy.pojos.responsepojos.BookingResponse;
+
 public class PayloadManager {
+
+    // The responsibility of POJO is to do serialization and deserialization
+
+    static Gson gson;
+    Faker faker;
+
+    // Serialization
+    // Convert the Java Object into the JSON String to use as a Payload
+
+    // Valid booking
+    public static String createPayloadBookingAsString(){
+        Booking booking = new Booking();
+        booking.setFirstName("Lucky");
+        booking.setLastName("Dutta");
+        booking.setTotalprice(112);
+        booking.setDepositpaid(true);
+
+        BookingDates bookingDates = new BookingDates();
+        bookingDates.setCheckin("2024-02-01");
+        bookingDates.setCheckout("2024-02-05");
+        booking.setBookingDates(bookingDates);
+        booking.setAdditionalneeds("Breakfast");
+
+        System.out.println(booking);
+        return gson.toJson(booking);
+
+        // The java object will be converted to Json format like below
+
+        //{
+            //"firstname" : "Adarsh1",
+               // "lastname" : "Suresh1",
+                //"totalprice" : 4100,
+               // "depositpaid" : true,
+                //"bookingdates" : {
+            //"checkin" : "2025-07-02",
+                   // "checkout" : "2025-07-27"
+       // },
+           // "additionalneeds" : "Breakfast"
+        //}
+    }
+
+    // invalid booking
+    public String createPayloadBookingAsStringWrongBody(){
+        Booking booking = new Booking();
+        booking.setFirstName("汉字, /漢字");
+        booking.setLastName("/漢字");
+        booking.setTotalprice(112);
+        booking.setDepositpaid(true);
+
+        BookingDates bookingDates = new BookingDates();
+        bookingDates.setCheckin("5025-02-01");
+        bookingDates.setCheckout("5025-02-05");
+        booking.setBookingDates(bookingDates);
+        booking.setAdditionalneeds("汉字");
+
+        return null;
+    }
+
+    // Valid booking with dynamic data
+    public String createPayloadBookingFakerJs(){
+        //This option is used when you dynamically generate the first name.
+        //First way-> lastname and other variables through faker
+        //faker = new Faker();
+
+        // Second way
+        //Method with the dynamic data we use
+        //We will fetch the data from Excel file
+        // We will use APACHE POI
+        //Store the value of Firstname, Lastname and everything and then->
+        // ->we will verify from the response
+
+        return null;
+    }
+
+    // Deserialization
+    // Convert JSON String to Java Objects
+
+    public static BookingResponse bookingResponseJava(String responseString){
+        gson = new Gson();
+        BookingResponse bookingResponse = gson.fromJson(responseString,BookingResponse.class);
+        return bookingResponse;
+    }
+    
+
 }
--- src/test/java/com/thetestingacademy/tests/restfulBooker/CRUD/TestCreateBooking.java
+++ src/test/java/com/thetestingacademy/tests/restfulBooker/CRUD/TestCreateBooking.java
@@ -1,6 +1,63 @@
 package com.thetestingacademy.tests.restfulBooker.CRUD;
 
-public class TestCreateBooking {
+import com.thetestingacademy.base.BaseTest;
+import com.thetestingacademy.endpoints.APIConstants;
+import com.thetestingacademy.modules.PayloadManager;
+import com.thetestingacademy.pojos.responsepojos.BookingResponse;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.restassured.RestAssured;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCreateBooking extends BaseTest {
+
+    @Test(groups = "reg", priority = 1)
+    @Owner("Pramod Dutta")
+    @Description("TC#INT1- Step1 . Verify that booking can be created")
+    public void testCreateBookingPost_Positive(){
+
+        //Part1-Setup and making the request
+
+        //Part3-Verification and Validation via Assertj, TESTNG
+
+        requestSpecification.basePath(APIConstants.CREATE_UPDATE_BOOKING_URl);
+        response= RestAssured.given(requestSpecification)
+                .when().body(PayloadManager.createPayloadBookingAsString())
+                .log().all().post();
+
+        //Part2-Extraction
+        BookingResponse bookingResponse = PayloadManager.bookingResponseJava(response.asString());
+
+
+        //Part3-Verification and Validation via Assertj, TESTNG
+        assertActions.verifyStatusCode(response, 200);
+        assertActions.verifyIntegerKeyNotNull(bookingResponse.getBookingid());
+        assertActions.verifyStringKey(bookingResponse.getBooking().getFirstName(),"Lucky");
+
+    }
+
+    @Test(groups = "reg", priority = 1)
+    @Owner("Pramod Dutta")
+    @Description("TC#INT1- Step1 . Verify that booking can't be created, when payload is null")
+    public void testCreateBookingPost_Negative(){
+
+        //Part1-Setup and making the request
+
+        //Part3-Verification and Validation via Assertj, TESTNG
+
+        requestSpecification.basePath(APIConstants.CREATE_UPDATE_BOOKING_URl);
+        response= RestAssured.given(requestSpecification)
+                .when().body("{}")
+                .log().all().post();
+
+        //Part2-Extraction
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(500);
+
+
+
+    }
 
 
 }
